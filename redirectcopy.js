53986e43762d528c119afc61cb06f3d76f6852b3
Seotopgoogle <142563509+Seotopgoogle@users.noreply.github.com>
2024-11-12 15:28:46 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Bản Quyền Thuộc Về : <a href='https://vnptcanthonet.com/'>https://vnptcanthonet.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Bản Quyền Thuộc Về : <a href='https://seotop.vn/'>https://seotop.vn/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
